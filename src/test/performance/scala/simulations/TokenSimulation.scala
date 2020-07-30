757f881a1e08b55f34440cee1fe1b7bb2f8b4e2b
Scott G <scottg489@gmail.com>
2020-07-29 19:35:37 -0700

    Add performance test for token endpoint
    This adds a performance testing framework and an initial basic test for
    the token endpoint.
    
    Note that this still needs to be configured to run in the CI build.

---
+++ src/test/performance/scala/simulations/TokenSimulation.scala
@@ -0,0 +1,27 @@
+import io.gatling.core.Predef._
+import io.gatling.core.scenario.Simulation
+import io.gatling.core.structure.ScenarioBuilder
+import io.gatling.http.Predef._
+import io.gatling.http.protocol.HttpProtocolBuilder
+import util.ConfigUtil
+
+class TokenSimulation extends Simulation {
+
+  private val baseUrl: String = ConfigUtil.getFromConfig("baseUri")
+
+  private val httpProtocol: HttpProtocolBuilder = http
+    .baseUrl(baseUrl)
+  private val request = http("Token request")
+    .get("/chat/token")
+  private val scn: ScenarioBuilder = scenario("BasicSimulation")
+    .exec(request)
+
+  setUp(
+    scn.inject(atOnceUsers(100))
+  )
+    .protocols(httpProtocol)
+    .assertions(
+      global.responseTime.mean.lt(500),
+      global.failedRequests.count.is(0)
+    )
+}
