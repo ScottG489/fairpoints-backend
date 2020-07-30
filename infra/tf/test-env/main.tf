7e0596bdc87a002ba3a99a0d85a0ed5c39b875fa
Scott G <scottg489@gmail.com>
2020-07-30 00:41:46 -0700

    Keep spot instance requests forever
    Because we weren't setting an expiration time on spot instance requests
    they would expire after 7 days. This would cause future deploys to think
    they need to make another request which then spins up another instance
    while the previous instance still exists. In this new version of the
    helper module they are set to never expire.

--- infra/tf/test-env/main.tf
+++ infra/tf/test-env/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 module "helpers_spot_instance_ssh" {
   source = "ScottG489/helpers/aws//modules/spot_instance_ssh"
-  version = "0.0.4"
+  version = "0.0.5"
   name = "${random_id.name_prefix.hex}.com"
   instance_type = var.instance_type
   spot_type = var.spot_type
